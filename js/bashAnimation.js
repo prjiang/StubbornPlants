374134bc22bbd4b257ace13d702e8b37a688f41c
P.R. Jiang <59597823+prjiang@users.noreply.github.com>
2020-10-19 23:38:12 +0800

    Add files via upload

---
+++ js/bashAnimation.js
@@ -0,0 +1,28 @@
+var myDescription = "Plan your life, Plant your life.";
+
+var textArray = myDescription.split("");
+var loopTimer;
+var doneShowingText = false;
+var setContent;
+
+
+function frameLooper() {
+    if (textArray.length > 0 && !doneShowingText) {
+        $('#myTypingText')[0].innerHTML += textArray.shift();
+    } else {
+        clearTimeout(loopTimer);
+        doneShowingText = true;
+        // console.log("done");
+    }
+    loopTimer = setTimeout('frameLooper()', 75);
+}
+
+
+$(document).ready(function () {
+    setContent = $('#content')[0].outerHTML = frameLooper();
+});
+
+/*
+var lastModDate = new Date(document.lastModified);
+    document.write("最後修改日期：" +lastModDate.toDateString());
+*/
